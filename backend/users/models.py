d05f4c63d543eb89f086087c89ac71ddfea465fe
Nahom Zewdu <nahiyo86@gmail.com>
2024-10-21 16:26:35 +0300

    Modified user model to be Custome and changed model fields

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,10 +1,25 @@
+from datetime import datetime
+
+from django.utils import timezone
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 from django.db import models
 
+from .managers import UserManager
 # Create your models here.
 
-class User(AbstractUser):
+class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
-    nickname = models.CharField(max_length=15)
+    username = models.CharField(max_length=25)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
     
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.username
