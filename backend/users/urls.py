2b28c375a2e49b31762f9954fa985450bf28f408
Nahom Zewdu <nahiyo86@gmail.com>
2024-10-21 16:34:41 +0300

    Added endpoint for login view

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,10 @@
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from django.urls import path
+from .jwt_serializers import EmailTokenObtainPairSerializer
 from .views import UserCreateView
 
 urlpatterns = [
-    path('register', UserCreateView.as_view(), name='register')
+    path('register', UserCreateView.as_view(), name='register'),
+    path('login', TokenObtainPairView.as_view(serializer_class=EmailTokenObtainPairSerializer), name='token_obtain_pair'),
 ]
