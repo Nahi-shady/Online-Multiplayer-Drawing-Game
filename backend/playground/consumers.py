94323b99157756692f1aa813f11233369807c20b
Nahom Zewdu <nahiyo86@gmail.com>
2024-10-24 01:20:35 +0300

    Added disconnect, receive and chat_message async functions for GameConsumer consumer

--- backend/playground/consumers.py
+++ backend/playground/consumers.py
@@ -13,3 +13,30 @@ class GameConsumer(AsyncWebsocketConsumer):
         )
         
         await self.accept()
+        
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+    async def receive(self, text_data):
+        json_text = json.load(text_data)
+        message = json_text['message']
+        
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'text-message',
+                'message': message,
+            }
+        )
+        
+    async def chat_message(self, event):
+        message = event['message']
+        
+        await self.send(
+            text_data=json.dumps(
+                {'message': message}
+                )
+            )
