9cc063d82dd49d91a3b7b3cf90417d3b8166ac5c
Nahom Zewdu <nahiyo86@gmail.com>
2024-10-30 21:15:28 +0300

    Removed playground app

--- backend/playground/routing.py
+++
@@ -1,8 +0,0 @@
-# routing.py
-from django.urls import path, re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    path('ws/game/<str:room_name>/', consumers.GameConsumer.as_asgi()),
-    re_path('ws/game/test/', consumers.TestConsumer.as_asgi()),
-]
