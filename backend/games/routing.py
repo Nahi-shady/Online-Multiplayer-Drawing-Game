fce7203eb68cc36094ab7d11116070120ebc1ca7
Nahom Zewdu <nahiyo86@gmail.com>
2024-11-01 00:22:08 +0300

    Edited the routings url path for websocket connection

--- backend/games/routing.py
+++ backend/games/routing.py
@@ -3,5 +3,5 @@ from django.urls import path, re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/game/<str:room_name>/', consumers.GameConsumer.as_asgi()),
+    path('ws/game/<str:room_code>', consumers.GameConsumer.as_asgi()),
 ]
