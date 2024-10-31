36a5970bd2097b2ecc2f62ceba28e3b6402c3373
Nahom Zewdu <nahiyo86@gmail.com>
2024-10-31 11:34:21 +0300

    Added a field in room model to set the max number of players

--- backend/games/models.py
+++ backend/games/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from users.models import User
 from .managers import RoomManager
-class Room(models.Model):
 
+class Room(models.Model):
     is_private = models.BooleanField(default=True)
-    unique_link = models.URLField(blank=True, null=True)
+    unique_code = models.CharField(max_length=8, blank=True, null=True, unique=True)
     current_players_count = models.IntegerField(default=0)
+    max_players = models.IntegerField(default=14)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -14,12 +14,13 @@ class Room(models.Model):
     def __str__(self):
         return f"{'Private' if self.is_private else 'Public'} Room (ID: {self.id})"
 
+
 class Player(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=15, blank=False, null=False)
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='players')
-    turn_order = models.IntegerField()  # Order in which the player takes their turn
-    is_active = models.BooleanField(default=True)  # To track if the player is currently in the game
-    score = models.IntegerField(default=0)  # Track player score
+    turn_order = models.IntegerField(default=0)  # Player's turn order within the room
+    is_active = models.BooleanField(default=True)
+    score = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.user.username} in Room {self.room.id}"
+        return f"{self.name} in Room {self.room.id}"
