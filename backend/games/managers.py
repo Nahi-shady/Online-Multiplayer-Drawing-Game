7f27b844db91f5985dbb32ad71ecdd6ebbefa86b
Nahom Zewdu <nahiyo86@gmail.com>
2024-10-31 11:35:47 +0300

    Optimized the Room Manager to handle creating and joining rooms

--- backend/games/managers.py
+++ backend/games/managers.py
@@ -1,12 +1,17 @@
-from django.contrib.auth.base_user import BaseUserManager
-from django.utils.translation import gettext_lazy as _
+from django.db import models
 from django.utils.crypto import get_random_string
 
-class RoomManager(BaseUserManager):
-    def create_room(self, is_private):
-        room = self.model(is_private=is_private)
-        if is_private:
-            room.unique_link = f"http://127.0.0.1:8000/games/room/{get_random_string(16)}"
-        room.save()
-        
-        return room
+class RoomManager(models.Manager):
+    def create_private_room(self, max_players=14):
+        unique_code = get_random_string(length=8)  # Generate a unique code for private rooms
+        return self.create(is_private=True, unique_code=unique_code, max_players=max_players)
+
+    def create_public_room(self, max_players=14):
+        return self.create(is_private=False, max_players=max_players)
+
+    def get_public_room(self):
+        # Find the first active public room that has space for more players
+        return self.filter(is_private=False, is_active=True).exclude(current_players_count__gte=models.F('max_players')).first()
+
+    def get_private_room(self, unique_code):
+        return self.filter(is_private=True, unique_code=unique_code, is_active=True).first()
