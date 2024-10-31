8889d291fe16205124e0cfc2289d62dc8596854b
Nahom Zewdu <nahiyo86@gmail.com>
2024-10-31 11:36:35 +0300

    Modified serializers of the room and player model

--- backend/games/serializers.py
+++ backend/games/serializers.py
@@ -1,14 +1,15 @@
-from rest_framework.validators import UniqueValidator
-from rest_framework import serializers
+# serializers.py
 
+from rest_framework import serializers
 from .models import Room, Player
 
-class RoomModelSerializer(serializers.ModelSerializer):
+class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ['is_private']
-        
-    def create(self, data):
-        room = Room.objects.create_room(is_private=data['is_private'])
-        
-        return room
+        fields = ['id', 'is_private', 'unique_code', 'current_players_count', 'max_players', 'is_active']
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ['id', 'name', 'room', 'turn_order', 'is_active', 'score']
