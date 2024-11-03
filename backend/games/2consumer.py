b02d80eaea3f7b3d39e57ea05eb84756422cccda
Nahom Zewdu <nahiyo86@gmail.com>
2024-11-03 17:47:41 +0300

    Modified websocket consumer game handling logic

--- backend/games/2consumer.py
+++ backend/games/2consumer.py
@@ -9,9 +9,7 @@ from .models import Room, Player
 
 class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.score_set = 450
         self.player_id = int(self.scope['url_route']['kwargs']['player_id'])
-        
         self.room_id = int(self.scope['url_route']['kwargs']['room_id'])
         self.room_group_name = f'room_{self.room_id}'
         
@@ -30,11 +28,7 @@ class GameConsumer(AsyncWebsocketConsumer):
             }
         )
         
-        players = await self.get_players_by_order()
-        await self.send(json.dumps({
-            "type": "player_list",
-            "players": [player.to_dict() for player in players],
-        }))
+        await self.update_leaderboard()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
@@ -48,44 +42,49 @@ class GameConsumer(AsyncWebsocketConsumer):
                 'type': 'player_left'
             }
         )
+        await self.update_leaderboard()
         
     async def receive(self, text_data):
         data = json.loads(text_data)
         message_type = data.get('type')
         
-        if message_type == 'guess':
+        if message_type == "guess":
             await self.handle_guess(data.get('guess'))
         elif message_type == "next_turn":
             await self.start_next_turn()
         
     # game logic
     async def handle_guess(self, guess):
-           room = await self.get_room()
-           
-           if room and guess.lower() == room.current_word.lower():
-                await self.update_scores(self.player_id)
-                player = await self.get_player()
-                await self.send(
-                    json.dumps(
-                        {
-                            'type': 'correct_guess',
-                            'player_id': player.id,
-                            'player_name': player.name
-                        }
-                    )
-                )
-       
-    async def update_scores(self, player_id):
-        player = await sync_to_async(Player.objects.get)(id=player_id)
-        player.score = F['score'] + self.score_set
+            room = await self.get_room()
+            player = await self.get_player()
+            guess_type = ''
+            if room and guess.lower() == room.current_word.lower():
+                await self.update_scores()
+                guess_type = 'correct_guess'
+            else:
+                guess_type = 'wrong_guess'
+            
+            await self.send(
+                json.dumps({
+                        'type': guess_type,
+                        'player_id': player.id,
+                        'player_name': player.name,
+                        'guess': guess,
+                    }))
+
+    async def update_scores(self):
+        room = await self.get_room()
+        player = await self.get_player()
+        
+        player.score = F['score'] + room.score_pool
         await sync_to_async(player.save)()
         
-        room = await self.get_room()
         drawer = room.current_drawer
-        drawer.score = F['score'] + 30
+        drawer.score = F['score'] + (room.score_pool//room.current_players_count)
         await sync_to_async(drawer.save)()
         
-        self.score_set -= 33
+        room.score_pool = F['score_pool'] - 33
+        await sync_to_async(room.save)()
         
         await self.update_leaderboard()
         
@@ -105,18 +104,17 @@ class GameConsumer(AsyncWebsocketConsumer):
         await self.set_next_drawer()
         room = await self.get_room()
         
-        # Reset the score_set, word and hints for the next round
+        # Reset the score_set and word for the next round
         # room.current_word = random.choice(room.word_list)
-        # room.hint_letters = 0
-        # await sync_to_async(room.save)()
-        self.score_set = 450
+        room.score_pool = 450
+        await sync_to_async(room.save)()
     
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 "type": "new_turn",
                 "drawer_name": room.current_drawer.name,
-                # "word_length": len(room.current_word),
+                "word_length": len(room.current_word),
             }
         )
         await self.start_turn_timer()
@@ -125,7 +123,7 @@ class GameConsumer(AsyncWebsocketConsumer):
         for remaining in range(45, 0, -1):
             await asyncio.sleep(1)
             
-            if remaining in {30, 15}:
+            if remaining in {25, 10}:
                 await self.provide_hint()
                 
             if remaining == 0:
@@ -185,12 +183,18 @@ class GameConsumer(AsyncWebsocketConsumer):
                 'type': 'hint_update',
                 'hint': event['hint']
             }))
-        
-        
+    async def new_turn(self, event):
+        await self.send(json.dumps({
+            "type": "new_turn",
+            "drawer_id": event["drawer_id"],
+            # "word_length": event["word_length"],
+        }))   
+              
     # helper
     @sync_to_async
     async def get_player(self):
         return Player.objects.get(id=self.player_id)
+    
     @sync_to_async
     async def get_room(self):
         return Room.objects.get(id=self.room_id)
@@ -198,16 +202,10 @@ class GameConsumer(AsyncWebsocketConsumer):
     @sync_to_async
     async def get_players_in_order(self):
         room = await self.get_room()
-        return list(Player.objects.filter(room=room).order_by("turn_order"))
+        return list(room.players.order_by("turn_order"))
     
+    @sync_to_async
     async def set_next_drawer(self):
-        players = await self.get_players_in_order()
         room = await self.get_room()
-        current_drawer_id = room.current_drawer.id
-        
-        drawer_idx = next((i for i, p in enumerate(players) if p.id == current_drawer_id), -1)
-        next_idx = (drawer_idx + 1) % len(players)
-        
-        room.current_drawer = players[next_idx]
-        await sync_to_async(room.save)()
+        await room.set_next_drawer()
         
