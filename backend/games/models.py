d7098d9ae901897769c2979b649840ef593057f7
Nahom Zewdu <nahiyo86@gmail.com>
2024-11-10 22:33:09 +0300

    removed set_next_drawer method from Room model

--- backend/games/models.py
+++ backend/games/models.py
@@ -1,5 +1,8 @@
+from asgiref.sync import sync_to_async
+
 from django.db import models
 from django.shortcuts import get_object_or_404
+
 from .managers import RoomManager
 
 class Room(models.Model):
@@ -10,31 +13,13 @@ class Room(models.Model):
     current_players_count = models.IntegerField(default=0)
     current_word = models.CharField(default='illustra', blank=True, null=False)
     score_pool = models.IntegerField(default=450, blank=True, null=False)
-    turn_count = models.IntegerField(max_length=2, blank=True, null=False)
+    turn_count = models.IntegerField(default=0, blank=False, null=False)
     
     max_players = models.IntegerField(default=14)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    objects = RoomManager()
-
-    def set_next_drawer(self):
-        players = list(self.players.filter(is_active=True).order_by('turn_order'))
-        
-        if players:
-            if self.current_drawer and self.current_drawer in players:
-                index = players.index(self.current_drawer)
-                next_index = (index + 1) % len(players)
-            else:
-                next_index = 0
-                
-            self.current_drawer = players[next_index]
-            self.save()
-            
-            return self.current_drawer
-        else:
-            self.current_drawer = None
-            self.save()
+    objects = RoomManager()       
     
     def __str__(self):
         return f"{'Private' if self.is_private else 'Public'} Room (ID: {self.id})"
