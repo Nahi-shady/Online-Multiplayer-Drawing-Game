1a9da35a5a0d0d88977fdc76b1ec9d3e2a4504d7
Nahom Zewdu <nahiyo86@gmail.com>
2024-11-23 16:28:40 +0300

    Removed LeaveRoomView from urlpatterns list

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -1,10 +1,9 @@
 # urls.py
 
 from django.urls import path
-from .views import CreateRoomView, JoinRoomView, LeaveRoomView
+from .views import CreateRoomView, JoinRoomView
 
 urlpatterns = [
     path('create-room', CreateRoomView.as_view(), name='create-room'),
     path('join-room', JoinRoomView.as_view(), name='join-room'),
-    path('leave-room', LeaveRoomView.as_view(), name='leave-room'),
 ]
