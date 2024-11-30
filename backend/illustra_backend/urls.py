333b178550b1c002889de31dfa5bbbc02cafab09
Nahom Zewdu <nahiyo86@gmail.com>
2024-11-30 23:54:38 +0300

    Modified url route for games app

--- backend/illustra_backend/urls.py
+++ backend/illustra_backend/urls.py
@@ -9,7 +9,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('', include('games.urls')),
+    path('games/', include('games.urls')),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
