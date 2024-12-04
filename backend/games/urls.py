5ac0d658bd184e99eaf87f75e1b4c80beeb80745
Nahom Zewdu <nahiyo86@gmail.com>
2024-12-04 12:47:14 +0300

    Added a url path to receive GET requests for csrf token

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -1,9 +1,10 @@
 # urls.py
 
 from django.urls import path
-from .views import CreateRoomView, JoinRoomView
+from .views import CreateRoomView, JoinRoomView, GetCsrfTokenView
 
 urlpatterns = [
+    path('get-csrf-token/', GetCsrfTokenView.as_view(), name='get-csrf-token'),
     path('create-room', CreateRoomView.as_view(), name='create-room'),
     path('join-room', JoinRoomView.as_view(), name='join-room'),
 ]
