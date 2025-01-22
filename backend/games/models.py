7299323350d41e108898c0ecf1e9b1ae5d62718d
Nahom Zewdu <nahiyo86@gmail.com>
2025-01-22 20:30:42 +0300

    set current drawer field on Room model to null on player deletion and change max_player count to 7

--- backend/games/models.py
+++ backend/games/models.py
@@ -9,7 +9,7 @@ class Room(models.Model):
     is_private = models.BooleanField(default=True)
     unique_code = models.CharField(max_length=8, blank=True, null=True, unique=True)
     
-    current_drawer = models.ForeignKey('Player', on_delete=models.CASCADE, blank=True, null=True, related_name='drawer_room')
+    current_drawer = models.ForeignKey('Player', on_delete=models.SET_NULL, blank=True, null=True, related_name='drawer_room')
     current_players_count = models.IntegerField(default=0)
     current_word = models.CharField(default='', blank=True, null=False)
     guess_count = models.IntegerField(default=0, blank=False, null=False)
@@ -17,12 +17,12 @@ class Room(models.Model):
     score_pool = models.IntegerField(default=450, blank=True, null=False)
     on = models.BooleanField(default=False)
     
-    max_players = models.IntegerField(default=14)
+    max_players = models.IntegerField(default=7)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     objects = RoomManager()       
-    
+
     def __str__(self):
         return f"{'Private' if self.is_private else 'Public'} Room (ID: {self.id})"
 
