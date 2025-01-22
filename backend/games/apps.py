9910660d14b9766fefc6860ca925a73ea5e0e213
Nahom Zewdu <nahiyo86@gmail.com>
2025-01-22 22:50:25 +0300

    Registered shutdown_handler in Games app

--- backend/games/apps.py
+++ backend/games/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from .shutdown import shutdown_handler
+import signal
 
-
-class GamesConfig(AppConfig):
+class GameAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'games'
+
+    def ready(self):
+        # Register shutdown signals
+        signal.signal(signal.SIGINT, shutdown_handler)  # Handle Ctrl+C
+        signal.signal(signal.SIGTERM, shutdown_handler)  # Handle stop command
