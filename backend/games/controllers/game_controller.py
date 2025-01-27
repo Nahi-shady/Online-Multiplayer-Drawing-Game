6ec010cc5032cf33dd9b53addee9852f3b36ff1a
Nahom Zewdu <nahiyo86@gmail.com>
2025-01-27 17:57:05 +0300

    Added the definition of the GameController controller

--- backend/games/controllers/game_controller.py
+++ backend/games/controllers/game_controller.py
@@ -12,3 +12,8 @@ from django.db.models import F
 from .models import Room, Player
 from .word_pool import EASY_WORDS, HARD_WORDS, FUN
 
+class GameController():
+    async def __init__(self, room_id):
+        self.room_id = room_id
+        self.room_group_name = f'room_{self.room_id}'
+        
