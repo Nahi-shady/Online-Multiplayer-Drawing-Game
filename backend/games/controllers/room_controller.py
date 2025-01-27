bb39cc887bdf6542c2375ef0d476f668f6c03c9e
Nahom Zewdu <nahiyo86@gmail.com>
2025-01-27 18:34:01 +0300

    Created base implementation for Room Controller class

--- backend/games/controllers/room_controller.py
+++ backend/games/controllers/room_controller.py
@@ -11,3 +11,17 @@ from django.db.models import F
 
 from .models import Room, Player
 from .word_pool import EASY_WORDS, HARD_WORDS, FUN
+
+class RoomControler():
+    def __init__(self, room_id: int) -> None:
+        self.room_id = room_id
+        self.room = await self.get_room()
+        
+        self.drawer = None
+        self.players_count = self.room.current_players_count
+        self.current_word = ''
+        self.guess_count = 0
+        self.turn_count = 0
+        self.score_pool = 450
+        self.on = False
+        
