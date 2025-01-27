ec6f9305ce096a590596927f1753e0542962ef6e
Nahom Zewdu <nahiyo86@gmail.com>
2025-01-27 17:55:41 +0300

    Created a directory for controllers

---
+++ backend/games/controllers/room_controller.py
@@ -0,0 +1,13 @@
+import json
+import random
+import logging
+from asgiref.sync import sync_to_async
+import asyncio
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.exceptions import DenyConnection
+
+from django.db.models import F
+
+from .models import Room, Player
+from .word_pool import EASY_WORDS, HARD_WORDS, FUN
