2e8a53fcb0d3f5f383c7d268fd64e59b354e17f6
Nahom Zewdu <nahiyo86@gmail.com>
2025-01-29 01:57:35 +0300

    Handled a drawer leaving room event

--- backend/games/controllers/game_controller.py
+++ backend/games/controllers/game_controller.py
@@ -6,11 +6,16 @@ import asyncio
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.exceptions import DenyConnection
+from channels.layers import get_channel_layer
 
 from .player_controller import PlayerController
 from .room_controller import RoomController
 
 from .word_pool import EASY_WORDS, HARD_WORDS, FUN
+
+channel_layer = get_channel_layer()
+
+room_task = {}
 
 class GameController():
     async def __init__(self, room_id):
@@ -22,21 +27,54 @@ class GameController():
         
     async def player_joined(self, player_id: int) -> bool:
         if self.player_controller.player_joined(player_id):
-            await self.group_send(
-                {
+            await sync_to_async(channel_layer.group_send)(
+                self.room_group_name, {
                     'type': 'player_joined',
                     'player_id': player_id
-                })
+                }
+            )
             return True
         return False
     async def player_left(self, player_id: int) -> bool:
-        if self.player_controller.remove_player(player_id):
-            await self.group_send(
-                {
+        player = await self.player_controller.get_player(player_id)
+        drawer = await self.room_controller.get_drawer(player_id)
+        
+        if player and drawer and player == drawer:
+            await sync_to_async(channel_layer.group_send)(
+                self.room_group_name, {
+                    "type": "skip_turn",
+                 "message": "Drawer disconnected!"
+                })
+            
+            if self.room_id in room_task:
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {"type": "message", "message": "Drawer disconnected"}
+                )
+                
+                room_task[self.room_id].cancel()
+                del room_task[self.room_id]
+            else:
+                await self.channel_layer.group_send(
+                self.room_group_name,
+                    {"type": "message",
+                    "message": "not handled drawer leave!"}
+                )
+
+        
+        if await self.player_controller.remove_player(player_id):
+            await sync_to_async(channel_layer.group_send)(
+                self.room_group_name,{
                     'type': 'player_left',
                     'player_id': player_id
                 })
-            
-            return True
         
-        return False
+        if not await self.room_controller.is_active():
+            if self.room_id in room_task:
+                room_task[self.room_id].cancel()
+                del room_task[self.room_id]
+            
+            if not await self.room_controller.remove_room():
+                logging.info("Room was not removed")
+        
+        return True
