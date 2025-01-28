8cac47a262241107fa455d48a3f8ea49c71b874b
Nahom Zewdu <nahiyo86@gmail.com>
2025-01-29 01:04:42 +0300

    moved database operation logics from game_controller to player controller and room controller, also added a method to handle player disconnecting

--- backend/games/controllers/game_controller.py
+++ backend/games/controllers/game_controller.py
@@ -7,9 +7,9 @@ import asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.exceptions import DenyConnection
 
-from django.db.models import F
+from .player_controller import PlayerController
+from .room_controller import RoomController
 
-from .models import Room, Player
 from .word_pool import EASY_WORDS, HARD_WORDS, FUN
 
 class GameController():
@@ -17,17 +17,26 @@ class GameController():
         self.room_id = room_id
         self.room_group_name = f'room_{self.room_id}'
         
-    async def player_joined(self, player_id: int) -> None:
-        try:
-            player = await sync_to_async(Player.objects.get)(pk=player_id)
-            player.is_active = True
-            await player.save()
-        except Player.DoesNotExist:
-            logging.error(f'Player with id {player_id} does not exist.')
-            return
+        self.player_controller = PlayerController(self.room_id)
+        self.room_controller = RoomController(self.room_id)
         
-        await self.group_send(
-            {
-                'type': 'player_joined',
-                'player_id': player_id
-            })
+    async def player_joined(self, player_id: int) -> bool:
+        if self.player_controller.player_joined(player_id):
+            await self.group_send(
+                {
+                    'type': 'player_joined',
+                    'player_id': player_id
+                })
+            return True
+        return False
+    async def player_left(self, player_id: int) -> bool:
+        if self.player_controller.remove_player(player_id):
+            await self.group_send(
+                {
+                    'type': 'player_left',
+                    'player_id': player_id
+                })
+            
+            return True
+        
+        return False
