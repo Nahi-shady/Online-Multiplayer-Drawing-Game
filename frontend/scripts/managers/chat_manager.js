c9272dd81ed76761d38189d3b5890ad2e99ce910
Nahom Zewdu <nahiyo86@gmail.com>
2025-02-01 11:23:40 +0300

    Put Chat manager class in separate file for easier management

---
+++ frontend/scripts/managers/chat_manager.js
@@ -0,0 +1,63 @@
+// Chat Manager class
+
+export default class ChatManager {
+    constructor(wsManager) {
+        this.wsManager = wsManager;
+
+        this.chatInput = document.getElementById("chat-input");
+        this.chatSendButton = document.getElementById("send-message");
+        this.chatMessages = document.getElementById("chat-messages");
+
+        this.setupEventListeners();
+    }
+
+    setupEventListeners() {
+        // Send message on button click
+        this.chatSendButton.addEventListener("click", this.sendMessage.bind(this));
+
+        // Send message on Enter key
+        this.chatInput.addEventListener("keydown", (event) => {
+            if (event.key === "Enter") {
+                this.sendMessage.bind(this);
+            }
+        });
+    }
+
+    sendMessage() {
+        const message = this.chatInput.value.trim();
+        if (!message) return;
+
+        // Prepare message payload
+        const chatMessage = {
+            type: "guess",
+            guess: message,
+        };
+
+        // Send message via WebSocket
+        this.wsManager.sendMessage(chatMessage);
+
+        // Clear the input field
+        this.chatInput.value = "";
+    }
+
+    displayMessage(message) {
+        const messageElement = document.createElement("div");
+        messageElement.className = `chat-message`;
+        if (message.type === "guess") {
+            messageElement.textContent = `${message.name}: ${message.guess}`;
+            if (message.correct === true) {    
+                messageElement.className = 'green-text';
+            }
+        } else if (message.type === "player_joined") {
+            messageElement.textContent = `${message.id} joined room`;
+            messageElement.className = 'green-text';
+        } else if (message.type === "player_left") {
+            messageElement.textContent = `${message.id} left room`;
+            messageElement.className = 'red-text';
+        }
+        this.chatMessages.appendChild(messageElement);
+
+        // Scroll to the latest message
+        this.chatMessages.scrollTop = this.chatMessages.scrollHeight;
+    }
+}
