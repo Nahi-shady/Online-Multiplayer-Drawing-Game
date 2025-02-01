3255a5f63ea8bb0cf7e3d28a5fbf51881f00ecb5
Nahom Zewdu <nahiyo86@gmail.com>
2025-02-01 18:18:31 +0300

    Added a condition to make sure no private rooms with the same unique_code exist in db before creating one;

--- backend/games/managers.py
+++ backend/games/managers.py
@@ -3,7 +3,8 @@ from django.utils.crypto import get_random_string
 
 class RoomManager(models.Manager):
     def create_private_room(self, unique_code, max_players=7):
-        return self.create(is_private=True, unique_code=unique_code, max_players=max_players)
+        if not self.get_private_room(unique_code):
+            return self.create(is_private=True, unique_code=unique_code, max_players=max_players)
 
     def create_public_room(self, max_players=7):
         return self.create(is_private=False, max_players=max_players)
