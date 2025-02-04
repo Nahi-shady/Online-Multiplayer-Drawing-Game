b25850d6d2c678ddfad22b922a60b5231aec6bc6
Nahom Zewdu <nahiyo86@gmail.com>
2025-02-04 22:18:52 +0300

    Removed routing for CreateView since we do not need it, also renamed the endpoint for joining a room to follow RESTFul api principles

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -1,10 +1,9 @@
 # urls.py
 
 from django.urls import path
-from .views import CreateRoomView, JoinRoomView, GetCsrfTokenView
+from .views import RoomView, GetCsrfTokenView
 
 urlpatterns = [
     path('get-csrf-token/', GetCsrfTokenView.as_view(), name='get-csrf-token'),
-    path('create-room', CreateRoomView.as_view(), name='create-room'),
-    path('join-room', JoinRoomView.as_view(), name='join-room'),
+    path('rooms', RoomView.as_view(), name='rooms'),
 ]
