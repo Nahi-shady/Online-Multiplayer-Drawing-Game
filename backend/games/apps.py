148bb4aa6d52d51e2a4eb635ceaa3b316519d688
Nahom Zewdu <nahiyo86@gmail.com>
2025-02-07 19:27:22 +0300

    Removed logic to clear database before server shutdown

--- backend/games/apps.py
+++ backend/games/apps.py
@@ -1,12 +1,5 @@
 from django.apps import AppConfig
-from .shutdown import shutdown_handler
-import signal
 
 class GameAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'games'
-
-    def ready(self):
-        # Register shutdown signals
-        signal.signal(signal.SIGINT, shutdown_handler)  # Handle Ctrl+C
-        signal.signal(signal.SIGTERM, shutdown_handler)  # Handle stop command
