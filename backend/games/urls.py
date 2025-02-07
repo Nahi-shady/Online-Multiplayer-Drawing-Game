998ed4b301b1238bb35103db3146d1bce567c44e
Nahom Zewdu <nahiyo86@gmail.com>
2025-02-07 22:42:29 +0300

    ---

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .views import RoomView, GetCsrfTokenView
 
 urlpatterns = [
-    path('get-csrf-token', GetCsrfTokenView.as_view(), name='get-csrf-token'),
-    path('rooms', RoomView.as_view(), name='rooms'),
+    path('get-csrf-token/', GetCsrfTokenView.as_view(), name='get-csrf-token'),
+    path('rooms/', RoomView.as_view(), name='rooms'),
 ]
