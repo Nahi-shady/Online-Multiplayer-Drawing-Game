5e9d0a386639fef5bf9c6831f47e7edec07e74c4
Nahom Zewdu <nahiyo86@gmail.com>
2025-02-07 21:45:51 +0300

    Modified websocket url

--- backend/games/routing.py
+++ backend/games/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from .consumers import GameConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/game/(?P<room_id>\w+)/(?P<player_id>\w+)/$", GameConsumer.as_asgi()),
+    re_path(r"ws/(?P<room_id>\w+)/(?P<player_id>\w+)/$", GameConsumer.as_asgi()),
 ]
