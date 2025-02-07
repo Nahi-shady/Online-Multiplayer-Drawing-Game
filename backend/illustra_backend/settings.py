951759e91fc4d42315aa873204169fa902113718
Nahom Zewdu <nahiyo86@gmail.com>
2025-02-07 23:46:33 +0300

    ---

--- backend/illustra_backend/settings.py
+++ backend/illustra_backend/settings.py
@@ -145,9 +145,10 @@ CHANNEL_LAYERS = {
 
 # CORS configuration
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-       'http://127.0.0.1:5500',
-)
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+]
 CORS_ALLOW_CREDENTIALS = True
 
 # Update CSRF trusted origins for production as needed
