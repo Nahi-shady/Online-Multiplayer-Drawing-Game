5379a062e0cc5c838acf20c9da36efb0d385330e
Nahom Zewdu <nahiyo86@gmail.com>
2025-02-07 21:49:26 +0300

    Modified http url names

--- backend/illustra_backend/urls.py
+++ backend/illustra_backend/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('games/', include('games.urls')),
+    path('', include('games.urls')),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
