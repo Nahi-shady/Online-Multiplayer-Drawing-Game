b9e8fac163ab16323096ea33e3e82e28e34d47b0
Nahom Zewdu <nahiyo86@gmail.com>
2025-02-07 22:04:15 +0300

    rearagned list postion of installed apps

--- backend/illustra_backend/settings.py
+++ backend/illustra_backend/settings.py
@@ -25,8 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'corsheaders',
     'games',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
